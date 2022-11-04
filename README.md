077000d15bac2ee8250e37c02a80410f6778aa0e
Philip Nguyen <philipnguyen953@gmail.com>
2022-11-04 12:23:02 -0700

    added readme and navbar component

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # fitness-app
+
+This fitness app was developed to access fundamental fitness tools required for achieve
+success no matter your fitness standards.
+
+Functionality:
+    - Calorie Calculator
+    - Food & Exercise Tracker
+    - Diet and Exercise Plan Creator
+
+Technologies used:
+    - HTML and CSS
+    - JavaScript
+    - React
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+    <div className="container">
+        <nav className="Navbar">
+            <h1>Fitness</h1>
+            <ul>
+                <li>Home</li>
+                <li>Journal</li>
+                <li>Calculator</li>
+                <li>Planner</li>
+                <li>Profile</li>
+            </ul>
+        </nav>
+    </div>
+    )
+}
+
+export default Navbar;
