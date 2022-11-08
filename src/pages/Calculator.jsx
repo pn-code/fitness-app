0a24f81de973fde75151fa06cfa967efffbb5063
Philip Nguyen <philipnguyen953@gmail.com>
2022-11-08 14:37:32 -0800

    added calculator functionality and guide

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,38 +1,134 @@
 import React from "react";
+import "../styles/calculator-styles.css";
 
 const Calculator = () => {
     const [metric, setMetric] = React.useState(false);
-    const [calories, setCalories] = React.useState(0);
-    const [weight, setWeight] = React.useState(0)
+    const [calories, setCalories] = React.useState(null);
+    const [weight, setWeight] = React.useState(0);
 
     const handleChange = (e) => {
         setWeight(e.target.value);
-    }
-
-    const calculateCal = () => {
-        setCalories(() => metric === true ? Math.round(weight * 33.333333) : weight * 15);
     };
 
-    console.log(metric)
+    const calculateCal = () => {
+        if (
+            (metric === true && weight < 40) ||
+            (metric === false && weight < 80)
+        ) {
+            alert("Please enter a valid weight.");
+        } else {
+            setCalories(() =>
+                metric === true ? Math.round(weight * 33.333333) : weight * 15
+            );
+        }
+    };
+
     const changeMetric = () => {
-        setMetric(metric => !metric)
-    }
+        setMetric((metric) => !metric);
+    };
 
     return (
         <div className="container">
             <main className="Calculator">
                 <div className="form--container">
                     <form>
-                        <div>
-                            <button type="button" onClick={changeMetric}>{metric === true ? `Metric (kg)` : `Imperial (lb)`}</button>
+                        <div className="metric--container">
+                            <h3>Calorie Calculator</h3>
+                            <button
+                                className="metric-btn"
+                                type="button"
+                                onClick={changeMetric}
+                            >
+                                {metric === true
+                                    ? `Metric (kg)`
+                                    : `Imperial (lb)`}
+                            </button>
                         </div>
                         <label>Weight: </label>
-                        <input placeholder="weight in pounds" type="number" onChange={handleChange}/>
-                        <button type="button" onClick={calculateCal}>
-                            Submit
+                        <input
+                            id="weight--input"
+                            placeholder="weight in pounds"
+                            type="number"
+                            onChange={handleChange}
+                        />
+                        <button
+                            className="calculate-btn"
+                            type="button"
+                            onClick={calculateCal}
+                        >
+                            Calculate
                         </button>
                         <div className="calculator--output">
-                            Your daily calorie consumption required for maintenance is around {calories} calories.
+                            {calories && (
+                                <p>
+                                    <strong>
+                                        Your daily calorie consumption required
+                                        for maintenance is around {calories}{" "}
+                                        calories.
+                                    </strong>
+                                </p>
+                            )}
+
+                            <h3>How can I use this information?</h3>
+                            <p>
+                                Calorie calculators are almost never 100%
+                                accurate, but you can still use this tool as a
+                                reference point to reach your fitness goals.
+                            </p>
+
+                            <div className="calorie-plan">
+                                <h4>Maintenance</h4>
+                                <p>
+                                    If you feel that your weight is perfect as
+                                    it is, you should aim to hit your
+                                    maintenance calories.
+                                    <br></br>
+                                    <br></br>
+                                    {calories && (
+                                        <strong>
+                                            Aim for {calories} calories.
+                                        </strong>
+                                    )}
+                                </p>
+                            </div>
+                            <div className="calorie-plan">
+                                <h4>Bulk</h4>
+                                <p>
+                                    Otherwise, if you'd like to gain some
+                                    weight, you can add 300-500 calories to your
+                                    daily calorie consumption. It is not
+                                    recommended, however, to go overboard with
+                                    your calorie surplus.
+                                    <br></br>
+                                    <br></br>
+                                    {calories && (
+                                        <strong>
+                                            Aim for {calories + 300}-
+                                            {calories + 500} calories.
+                                        </strong>
+                                    )}
+                                </p>
+                            </div>
+                            <div className="calorie-plan">
+                                <h4>Cut</h4>
+                                <p>
+                                    Last, but not least, if you wish to lose
+                                    weight, you should remove 500 calories from
+                                    your maintenance calories to achieve
+                                    approximately 1 pound per week. Make sure
+                                    you are monitoring your weight loss and
+                                    increase the calorie deficit to match your
+                                    desired rate of weight loss.
+                                    <br></br>
+                                    <br></br>
+                                    {calories && (
+                                        <strong>
+                                            Aim for {calories - 1000}-
+                                            {calories - 500} calories.
+                                        </strong>
+                                    )}
+                                </p>
+                            </div>
                         </div>
                     </form>
                 </div>
