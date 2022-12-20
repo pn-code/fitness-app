1116dbf00e35834c00d459cf56a5f2e8b4b9be7f
Philip <philipnguyen953@gmail.com>
2022-12-20 12:57:04 -0800

    Linked client to server (plans)

--- client/src/pages/MyPlans.jsx
+++ client/src/pages/MyPlans.jsx
@@ -1,76 +1,78 @@
 import React from "react";
+import { useEffect } from "react";
 import Plan from "../components/Plan";
 import "../styles/my-plans.css";
 import Planner from "../components/Planner";
 
 const MyPlans = (props) => {
-    const { user, userData, setFetchData } = props;
-    const [savedPlans, setSavedPlans] = React.useState([]);
-    const [renderPlanner, setRenderPlanner] = React.useState(false);
+  const { user, userData, setFetchData } = props;
+  const [savedPlans, setSavedPlans] = React.useState([]);
+  const [renderPlanner, setRenderPlanner] = React.useState(false);
+  const API = `http://localhost:3000/api/plans`;
 
-    // const removePlan = async (e) => {
-    //     // Remove from savedEntries via setSavedEntries
-    //     setSavedPlans((prevSavedPlans) =>
-    //         prevSavedPlans.filter((plan) => plan.id !== e.target.className)
-    //     );
-    //     // Use setDoc to send data to database
-    //     if (user) {
-    //         await setDoc(
-    //             doc(db, "users", user.uid),
-    //             {
-    //                 plans: savedPlans.filter(
-    //                     (plan) => plan.id !== e.target.className
-    //                 ),
-    //             },
-    //             { merge: true }
-    //         );
-    //     }
-    // };
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => setSavedPlans(res.plans));
+  }, []);
 
-    // const planElements = savedPlans.map((plan) => (
-    //     <Plan planDisplay={plan} key={plan.id} id={plan.id} removePlan={removePlan}/>
-    // ));
+  // const removePlan = async (e) => {
+  //     // Remove from savedEntries via setSavedEntries
+  //     setSavedPlans((prevSavedPlans) =>
+  //         prevSavedPlans.filter((plan) => plan.id !== e.target.className)
+  //     );
+  //     // Use setDoc to send data to database
+  //     if (user) {
+  //         await setDoc(
+  //             doc(db, "users", user.uid),
+  //             {
+  //                 plans: savedPlans.filter(
+  //                     (plan) => plan.id !== e.target.className
+  //                 ),
+  //             },
+  //             { merge: true }
+  //         );
+  //     }
+  // };
 
-    return (
-        <div className="container">
-            <div className="MyPlans">
-                <header>
-                    <div className="MyPlans--info">
-                        <h2 className="page-header">
-                            {!renderPlanner
-                                ? `Training Plans`
-                                : `Create a Plan`}
-                        </h2>
-                        <button
-                            onClick={() =>
-                                setRenderPlanner(
-                                    (prevRenderPlanner) => !prevRenderPlanner
-                                )
-                            }
-                        >
-                            {!renderPlanner
-                                ? `Create New Plan`
-                                : `My Plans: ${savedPlans.length}`}
-                        </button>
-                    </div>
-                </header>
-                {!renderPlanner && (
-                    <main className="plans--container">
-                        {savedPlans.length === 0 && (
-                            <h4>You currently have no training plans.</h4>
-                        )}
-                        {/* {planElements} */}
-                    </main>
-                )}
-                {renderPlanner && (
-                    <Planner
-                        setSavedPlans={setSavedPlans}
-                        updateData={updateData}
-                    />
-                )}
-            </div>
-        </div>
-    );
+  console.log(savedPlans)
+  const planElements = savedPlans.map((plan) => (
+      <Plan plan={plan} key={plan._id} id={plan._id}/>
+  ));
+
+  return (
+    <div className="container">
+      <div className="MyPlans">
+        <header>
+          <div className="MyPlans--info">
+            <h2 className="page-header">
+              {!renderPlanner ? `Training Plans` : `Create a Plan`}
+            </h2>
+            <button
+              onClick={() =>
+                setRenderPlanner((prevRenderPlanner) => !prevRenderPlanner)
+              }
+            >
+              {!renderPlanner
+                ? `Create New Plan`
+                : `My Plans: ${savedPlans.length}`}
+            </button>
+          </div>
+        </header>
+        {!renderPlanner && (
+          <main className="plans--container">
+            {savedPlans.length === 0 && (
+              <h4>You currently have no training plans.</h4>
+            )}
+            {planElements}
+          </main>
+        )}
+        {renderPlanner && (
+          <Planner setSavedPlans={setSavedPlans}/>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default MyPlans;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const mongoose = require("mongoose");
-require('dotenv').config()
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const Plan = require("./models/Plan");
+require("dotenv").config();
 
 // Set up default mongoose connection
 const mongoDB = process.env.MONGO_URL;
@@ -14,15 +17,81 @@ const db = mongoose.connection;
 // Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World.")
-})
+  res.send("Hello World.");
+});
 
-app.get("/api/plans", (req, res) => {
-    
-})
+app.get("/api/plans", (req, res, next) => {
+  Plan.find({}, (err, plans) => {
+    if (err) {
+      next(err);
+    } else {
+      // Success!
+      res.json({
+        plans,
+      });
+    }
+  });
+});
+
+app.post("/api/plans", (req, res, next) => {
+  const planDetail = {
+    title: req.body.title,
+    emphasis: req.body.emphasis,
+    exercises: [
+      {
+        name: req.body.exercise1_name,
+        sets: req.body.exercise1_sets,
+        reps: req.body.exercise1_reps,
+      },
+      {
+        name: req.body.exercise2_name,
+        sets: req.body.exercise2_sets,
+        reps: req.body.exercise2_reps,
+      },
+      {
+        name: req.body.exercise3_name,
+        sets: req.body.exercise3_sets,
+        reps: req.body.exercise3_reps,
+      },
+      {
+        name: req.body.exercise4_name,
+        sets: req.body.exercise4_sets,
+        reps: req.body.exercise4_reps,
+      },
+      {
+        name: req.body.exercise5_name,
+        sets: req.body.exercise5_sets,
+        reps: req.body.exercise5_reps,
+      },
+      {
+        name: req.body.exercise6_name,
+        sets: req.body.exercise6_sets,
+        reps: req.body.exercise6_reps,
+      },
+      {
+        name: req.body.exercise7_name,
+        sets: req.body.exercise7_sets,
+        reps: req.body.exercise7_reps,
+      },
+    ],
+  };
+  const plan = new Plan(planDetail).save((err) => {
+    if (err) {
+      next(err);
+    } else {
+      res.json({
+        status: "Success",
+        planDetail,
+      });
+    }
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}.`)
-})
+  console.log(`Server is running on port: ${PORT}.`);
+});
