796e886bc9a29cae994786e9285468a70a5455f2
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-01 21:43:52 -0800

    Added Icons to features section

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import { BsCalculatorFill, BsJournalRichtext } from "react-icons/bs";
+import { BiRun } from "react-icons/bi";
 
 const LandingPage = () => {
     return (
         // Container
-        <div className="flex flex-col bg-[#040324] text-white ">
+        <div className="flex flex-col bg-[#040324] text-white px-10">
             {/* HERO SECTION */}
-            <div className="flex flex-col justify-center items-center my-6">
+            <div className="flex flex-col justify-center items-center my-6 mb-10">
                 <h2 className="font-semibold text-[16px] mb-2">
                     DISCOVER YOUR INNER STRENGTH
                 </h2>
-                <span className="text-[12px] mx-12 mb-2">
+                <span className="text-[12px] mb-2">
                     Your dream body has never been easier to reach.
                 </span>
-                <span className="text-[12px] mx-14 mb-6 text-[#FDCA15]">
+                <span className="text-[12px] mx-4 mb-6 text-[#FDCA15]">
                     Sign up now and enjoy fundamental fitness tools that are
                     guaranteed to work!
                 </span>
@@ -27,26 +29,33 @@ const LandingPage = () => {
             <div></div>
 
             {/* FEATURES SECTION */}
-            <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-8 mb-10">
                 {/* CALCULATE */}
-                <div>
-                    <img src="" alt="" />
+                <div className="flex gap-8">
+                    <div className="flex justify-center items-center bg-white w-[70px] h-[70px] rounded-xl">
+                        <BsCalculatorFill size={60} color="#6184FE" />
+                    </div>
                     <div>
                         <h2>CALCULATE</h2>
                         <span>Find your starting point.</span>
                     </div>
                 </div>
                 {/* PLAN */}
-                <div>
-                    <img src="" alt="" />
+                <div className="flex gap-8">
+                    <div className="flex justify-center items-center bg-white w-[70px] h-[70px] rounded-xl">
+                        <BiRun size={60} color="#6184FE" />
+                    </div>
                     <div>
                         <h2>PLAN</h2>
                         <span>Take control of your workouts.</span>
                     </div>
                 </div>
                 {/* TRACK */}
-                <div>
-                    <img src="" alt="" />
+                <div className="flex gap-8">
+                    <div className="flex justify-center items-center bg-white w-[70px] h-[70px] rounded-xl">
+                        <BsJournalRichtext size={60} color="#6184FE" />
+                    </div>
+
                     <div>
                         <h2>TRACK</h2>
                         <span>Add success to your to do list.</span>
