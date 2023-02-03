5cdcaf820969f7d0bcf581ac8979d07a8f2e8f38
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-02 18:35:13 -0800

    Added styles for larger media queries

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -72,7 +72,7 @@ const SignUp = () => {
                 </div>
 
                 <button
-                    className="bg-[#040324] text-white mt-5 px-5 py-2 rounded-lg hover:bg-white hover:text-[#040324] ease-in duration-150"
+                    className="bg-[#3731e0] text-white mt-5 px-5 py-2 rounded-lg hover:bg-white hover:text-[#040324] ease-in duration-150"
                     type="submit"
                 >
                     Register
