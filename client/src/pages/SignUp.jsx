80c93c403cbe6da9afd5fda1ea52ae61adc2be5e
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-02 18:28:35 -0800

    Restyled to match the theme of design

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,29 +4,85 @@ const SignUp = () => {
     const API = "http://localhost:3000/register";
 
     return (
-        <div>
-            <form action={API} method="POST">
-                <label htmlFor="first_name">First Name: </label>
-                <input id="first_name" name="first_name" type="text" />
-                <label htmlFor="last_name">Last Name: </label>
-                <input id="last_name" name="last_name" type="text" />
-                <label htmlFor="email">Email: </label>
-                <input id="email" name="email" type="email" />
-                <label htmlFor="username">Username: </label>
-                <input id="username" name="username" type="text" />
-                <label htmlFor="password">Password: </label>
-                <input id="password" name="password" type="password" />
-                <label htmlFor="confirm_password">Confirm Password: </label>
-                <input
-                    id="confirm_password"
-                    name="confirm_password"
-                    type="password"
-                />
-                <button type="submit">Register</button>
+        <div className="flex flex-col bg-[#040324] text-white px-10 gap-16">
+            <form
+                className="bg-[#040324] flex flex-col items-center justify-center gap-3"
+                action={API}
+                method="POST"
+            >
+                <h2 className="text-3xl text-center mb-4">SIGN UP</h2>
+                <div className="flex flex-col">
+                    <label htmlFor="first_name">First Name: </label>
+                    <input
+                        className="input-bl"
+                        id="first_name"
+                        name="first_name"
+                        type="text"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label htmlFor="last_name">Last Name: </label>
+                    <input
+                        className="input-bl"
+                        id="last_name"
+                        name="last_name"
+                        type="text"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label htmlFor="email">Email: </label>
+                    <input
+                        className="input-bl"
+                        id="email"
+                        name="email"
+                        type="email"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label htmlFor="username">Username: </label>
+                    <input
+                        className="input-bl"
+                        id="username"
+                        name="username"
+                        type="text"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label htmlFor="password">Password: </label>
+                    <input
+                        className="input-bl"
+                        id="password"
+                        name="password"
+                        type="password"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label htmlFor="confirm_password">Confirm Password: </label>
+                    <input
+                        className="input-bl"
+                        id="confirm_password"
+                        name="confirm_password"
+                        type="password"
+                    />
+                </div>
+
+                <button
+                    className="bg-[#040324] text-white mt-5 px-5 py-2 rounded-lg hover:bg-white hover:text-[#040324] ease-in duration-150"
+                    type="submit"
+                >
+                    Register
+                </button>
+                <Link to="/fitness-app/login">
+                    <span className="text-white text-[12px] sm:text-sm hover:text-gray-400">
+                        Already have an account? Log In.
+                    </span>
+                </Link>
             </form>
-            <Link to="/fitness-app/login">
-                Already have an account? Log in.
-            </Link>
         </div>
     );
 };
