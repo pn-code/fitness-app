f1f1d28bf6a54ab7146ef82c543667cf12b8becb
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-03 18:54:00 -0800

    Added conditional rendering of exercises

--- client/src/components/Planner.jsx
+++ client/src/components/Planner.jsx
@@ -6,7 +6,7 @@ const Planner = (props) => {
     const { setSavedPlans, updateData } = props;
     const [formPage, setFormPage] = useState(0);
     const [plan, setPlan] = useState({});
-    
+
     const API = "http://localhost:3000/plans";
 
     const handleChange = (e) => {
@@ -16,10 +16,14 @@ const Planner = (props) => {
     };
 
     const handleSubmit = (e) => {
-        // Save to array
-        setSavedPlans((prevSavedPlans) => [plan, ...prevSavedPlans]);
-        // Reset Form
-        setPlan({});
+        if (formPage < 2) {
+            setFormPage((page) => page + 1);
+        } else {
+            // Save to array
+            setSavedPlans((prevSavedPlans) => [plan, ...prevSavedPlans]);
+            // Reset Form
+            setPlan({});
+        }
     };
 
     return (
@@ -27,39 +31,84 @@ const Planner = (props) => {
             <span className="text-red-400">
                 The symbol * means that the input field is required.
             </span>
+
             <form
                 className="flex flex-col gap-4 items-start justify-start sm:items-center sm:justify-center bg-transparent"
                 action={API}
                 method="POST"
             >
-                <div>
-                    <label htmlFor="name">* Plan Name: </label>
-                    <input
-                        className="input-bl"
-                        id="name"
-                        name="name"
-                        type="text"
-                        onChange={handleChange}
-                        minLength={8}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="emphasis">* Emphasis: </label>
-                    <input
-                        className="input-bl"
-                        id="emphasis"
-                        name="emphasis"
-                        type="text"
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
+                {/* PAGE #1 */}
+                {formPage === 0 && (
+                    <>
+                        <div>
+                            <label htmlFor="name">* Plan Name: </label>
+                            <input
+                                className="input-bl"
+                                id="name"
+                                name="name"
+                                type="text"
+                                onChange={handleChange}
+                                minLength={8}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="emphasis">* Emphasis: </label>
+                            <input
+                                className="input-bl"
+                                id="emphasis"
+                                name="emphasis"
+                                type="text"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                    </>
+                )}
+
+                {formPage === 1 && (
+                    <>
+                        <div>
+                            <label htmlFor="name">Exercise Name: </label>
+                            <input
+                                className="input-bl-lg"
+                                id="name"
+                                name="name"
+                                type="text"
+                                onChange={handleChange}
+                                minLength={8}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="emphasis">Sets: </label>
+                            <input
+                                className="input-bl"
+                                id="emphasis"
+                                name="emphasis"
+                                type="text"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="emphasis">Reps: </label>
+                            <input
+                                className="input-bl"
+                                id="emphasis"
+                                name="emphasis"
+                                type="text"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                    </>
+                )}
 
                 <div className="btn--container">
                     <button
                         className="btn-blue"
-                        type="submit"
+                        type="button"
                         onClick={handleSubmit}
                     >
                         Submit
