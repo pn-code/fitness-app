e0358ebc52ce377d6e48a4d177761e2c7acedfbb
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-03 17:11:42 -0800

    Refactored to make styles and code cleaner

--- client/src/components/CaloriePlan.jsx
+++ client/src/components/CaloriePlan.jsx
@@ -5,32 +5,34 @@ const CaloriePlan = (props) => {
     const [isClicked, setIsClicked] = React.useState(false);
 
     const handleClick = () => {
-        setIsClicked(prevIsClicked => !prevIsClicked)
-    }
+        setIsClicked((IsClicked) => !IsClicked);
+    };
 
-    const displayHidden = (
-        <div className="CaloriePlan" onClick={handleClick}>
-            <h3>{planName}</h3>
-            <img src={imgSrc}/>
-            <span>Click Here</span>
+    return (
+        <div className="h-72">
+            {!isClicked ? (
+                <div
+                    className="h-full flex flex-col py-2 px-4 justify-center items-center cursor-pointer hover:bg-gray-200 hover:scale-110 duration-200 ease-in-out hover:text-black rounded-md"
+                    onClick={handleClick}
+                >
+                    <h3 className="text-lg">{planName}</h3>
+                    <img src={imgSrc} />
+                </div>
+            ) : (
+                <div className="h-full flex flex-col py-2 px-4 justify-center items-center cursor-pointer hover:bg-gray-200 hover:scale-110 duration-200 ease-in-out hover:text-black rounded-md" onClick={handleClick}>
+                    <h4 className="text-lg font-semibold mb-4">{planName}</h4>
+                    <p>
+                        {planInfo}
+                        <br></br>
+                        <br></br>
+                        <span>GOAL: </span>
+                        <span className="text-[#6d63f7]">{calorieGoal} calories.</span>
+                        
+                    </p>
+                </div>
+            )}
         </div>
     );
-
-    const displayVisible = (
-        <div className="CaloriePlan" onClick={handleClick}>
-            <h4>{planName}</h4>
-            <p>
-                {planInfo}
-                <br></br>
-                <br></br>
-                <strong>Aim for {calorieGoal} calories.</strong>
-            </p>
-        </div>
-    );
-
-
-
-    return <>{isClicked ? displayVisible : displayHidden}</>;
 };
 
 export default CaloriePlan;
