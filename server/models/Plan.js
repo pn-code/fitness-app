6bf45325a71db37f3bb3248fe52b66d165285c5b
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-04 17:04:52 -0800

    Refactored to include desc

--- server/models/Plan.js
+++ server/models/Plan.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PlanSchema = new Schema({
-    name: String,
-    author: { type: Schema.Types.ObjectId, ref: "User" },
-    emphasis: String,
-    exercises: [],
+	title: { type: String, require: true },
+	emphasis: { type: String, require: true },
+	desc: { type: String, require: true },
+	exercises: { type: [], require: true },
+	userId: { type: Schema.Types.ObjectId, ref: "User", require: true },
 });
 
 module.exports = mongoose.model("Plan", PlanSchema);
