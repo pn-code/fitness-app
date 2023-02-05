9b22aa013332eb5110954c60fc8daac3cede66fe
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-04 20:43:40 -0800

    Refactored planElements into main code

--- client/src/pages/MyPlans.jsx
+++ client/src/pages/MyPlans.jsx
@@ -7,77 +7,69 @@ import Planner from "../components/Planner";
 const API = `http://localhost:3000/plans/`;
 
 const MyPlans = (props) => {
-    const { user, userData, setFetchData } = props;
-    const [savedPlans, setSavedPlans] = React.useState([]);
-    const [renderPlanner, setRenderPlanner] = React.useState(false);
+  const { user, userData, setFetchData } = props;
+  const [savedPlans, setSavedPlans] = React.useState([]);
+  const [renderPlanner, setRenderPlanner] = React.useState(false);
 
-    // Loads savedPlans on start with information from database
-    useEffect(() => {
-        fetch(API)
-            .then((res) => res.json())
-            .then((res) => setSavedPlans(res.plans));
-    }, []);
+  // Loads savedPlans on start with information from database
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => setSavedPlans(res.plans));
+  }, []);
 
-    const deletePlan = async (id) => {
-        // Delete from savedEntries via setSavedEntries
-        setSavedPlans((prevSavedPlans) =>
-            prevSavedPlans.filter((plan) => id !== plan._id)
-        );
-        // // Delete selected plan from database
-        fetch(API + id, { method: "DELETE" });
-    };
-
-    // Map over savedPlans and display on the page
-    const planElements = savedPlans.map((plan) => (
-        <Plan
-            plan={plan}
-            key={plan._id}
-            id={plan._id}
-            deletePlan={deletePlan}
-        />
-    ));
-
-    return (
-        <div className="text-white mx-10">
-            <div className="MyPlans">
-                <header>
-                    <div className="flex justify-between items-center mb-6">
-                        <h2 className="text-2xl mt-3">
-                            {!renderPlanner
-                                ? `Training Plans`
-                                : `Create a Plan`}
-                        </h2>
-                        <button
-                        className="btn-blue-light"
-                            onClick={() =>
-                                setRenderPlanner(
-                                    (prevRenderPlanner) => !prevRenderPlanner
-                                )
-                            }
-                        >
-                            {!renderPlanner
-                                ? `Create New Plan`
-                                : `My Plans: ${savedPlans.length}`}
-                        </button>
-                    </div>
-                </header>
-                {!renderPlanner && (
-                    <main className="plans--container">
-                        {savedPlans.length === 0 && (
-                            <h4>You currently have no training plans.</h4>
-                        )}
-                        {planElements}
-                    </main>
-                )}
-                {renderPlanner && (
-                    <Planner
-                        setSavedPlans={setSavedPlans}
-                        deletePlan={deletePlan}
-                    />
-                )}
-            </div>
-        </div>
+  const deletePlan = async (id) => {
+    // Delete from savedEntries via setSavedEntries
+    setSavedPlans((prevSavedPlans) =>
+      prevSavedPlans.filter((plan) => id !== plan._id)
     );
+    // // Delete selected plan from database
+    fetch(API + id, { method: "DELETE" });
+  };
+
+  return (
+    <div className="text-white mx-10">
+      <div className="MyPlans">
+        <header>
+          <div className="flex justify-between items-center mb-6">
+            <h2 className="text-2xl mt-3">
+              {!renderPlanner ? `Training Plans` : `Create a Plan`}
+            </h2>
+            <button
+              className="btn-blue-light"
+              onClick={() =>
+                setRenderPlanner((prevRenderPlanner) => !prevRenderPlanner)
+              }
+            >
+              {!renderPlanner
+                ? `Create New Plan`
+                : `My Plans: ${savedPlans.length}`}
+            </button>
+          </div>
+        </header>
+        {!renderPlanner && (
+          <main className="plans--container">
+            {savedPlans.length === 0 && (
+              <h4>You currently have no training plans.</h4>
+            )}
+            <ul className="flex flex-col gap-6">
+              {savedPlans.map((plan) => (
+                <Plan
+                  plan={plan}
+                  key={plan._id}
+                  id={plan._id}
+                  deletePlan={deletePlan}
+                />
+              ))}
+            </ul>
+          </main>
+        )}
+        {renderPlanner && (
+          <Planner setSavedPlans={setSavedPlans} deletePlan={deletePlan} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default MyPlans;
