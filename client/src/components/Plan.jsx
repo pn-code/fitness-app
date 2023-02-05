45b0a53d3d3bfadca03689d2c320399973c71cbf
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-04 21:09:20 -0800

    Added viewHandler and styles

--- client/src/components/Plan.jsx
+++ client/src/components/Plan.jsx
@@ -1,44 +1,56 @@
 import { useState } from "react";
 
 const Plan = (props) => {
-	const { plan, id, deletePlan } = props;
-	const [view, setView] = useState(false);
+  const { plan, id, deletePlan } = props;
+  const [view, setView] = useState(false);
 
-	return (
-		// Show this when view is false...
-		<div className="bg-slate-700 px-6 py-3 rounded-md">
-			<div className="flex justify-between">
-				<div>
-					<h3 className="text-lg">{plan.title}</h3>
-					<span className="text-md">{plan.emphasis}</span>
-				</div>
-				<p>{plan.desc}</p>
-			</div>
+  const handleView = () => {
+    setView((view) => !view);
+  };
 
-			{view && (
-				<ul>
-					{plan.exercises.map((exercise) => {
-						if (exercise.name !== "") {
-							return (
-								<li>
-									<div>
-										<h4>{exercise.name}</h4>
-										<p>{`${exercise.sets} x ${exercise.reps}`}</p>
-									</div>
-								</li>
-							);
-						}
-					})}
-				</ul>
-			)}
+  return (
+    // Show this when view is false...
+    <div
+      onClick={handleView}
+      className="bg-slate-700 px-6 py-3 rounded-md cursor-pointer"
+    >
+      <div className="flex flex-col justify-between">
+        <div className="flex justify-between items-center mb-2">
+          <h3 className="text-lg font-bold">{plan.title}</h3>
+          <span className="text-lg font-semibold">{plan.emphasis}</span>
+        </div>
+        <p className="text mb-4">{plan.desc}</p>
+        {!view && <span className="text-xs text-center">Click to View</span>}
+      </div>
 
-			{/* <div className="btn--container">
-				<button className={id} onClick={() => deletePlan(id)}>
-					Remove Plan
-				</button>
-			</div> */}
-		</div>
-	);
+      {view && (
+        <ul className="flex flex-col gap-1">
+          <h4 className="font-semibold">Exercises:</h4>
+          {plan.exercises.map((exercise) => {
+            if (exercise.name !== "") {
+              return (
+                <li>
+                  <div className="flex justify-between">
+                    <h4>{exercise.name}</h4>
+                    <p>{`${exercise.sets} x ${exercise.reps}`}</p>
+                  </div>
+                </li>
+              );
+            }
+          })}
+        </ul>
+      )}
+
+      {view && (
+        <div className="flex flex-col">
+          <button className="bg-red-500 rounded-md mt-4 py-1 hover:bg-red-400" onClick={() => deletePlan(id)}>
+            Remove Plan
+          </button>
+          <span className="text-xs text-center mt-2">Click to Close</span>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Plan;
