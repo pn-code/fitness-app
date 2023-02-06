26ca7efb72688e7cd08bc82da68d9c28add4b547
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-05 21:24:17 -0800

    Updated delete endpoint to remove params

--- client/src/pages/MyPlans.jsx
+++ client/src/pages/MyPlans.jsx
@@ -7,76 +7,89 @@ import axios from "axios";
 const API = "http://localhost:3000/plans/";
 
 const MyPlans = ({ user, userData, setFetchData }) => {
-  const [savedPlans, setSavedPlans] = React.useState([]);
-  const [renderPlanner, setRenderPlanner] = React.useState(false);
+	const [savedPlans, setSavedPlans] = React.useState([]);
+	const [renderPlanner, setRenderPlanner] = React.useState(false);
 
-  // Loads savedPlans on start with information from database
-  useEffect(() => {
-    if (user) {
-      const fetchPlans = async () => {
-        const res = await axios.get(API + user?._id);
-        setSavedPlans(res.data.plans);
-      };
-      fetchPlans();
-    }
-  }, [user]);
+	const fetchPlans = async () => {
+		const res = await axios.get(API + user?._id);
+		setSavedPlans(res.data.plans);
+	};
 
-  const deletePlan = async (planId, planUserId) => {
-    // // Delete selected plan from database
-    await axios.delete(API + planId, { data: { userId: user._id, planUserId: planUserId } });
+	// Loads savedPlans on start with information from database
+	useEffect(() => {
+		if (user) {
+			fetchPlans();
+		}
+	}, [user]);
 
-    // Delete from savedEntries via setSavedEntries
-    setSavedPlans((prevSavedPlans) =>
-      prevSavedPlans.filter((plan) => planId !== plan._id)
-    );
-  };
+	const deletePlan = async (planId, planUserId) => {
+		// // Delete selected plan from database
+		await axios.delete(API, {
+			data: {
+				userId: user._id,
+				planUserId: planUserId,
+				currentPlanId: planId,
+			},
+		});
 
-  return (
-    <div className="text-white mx-10">
-      <div className="MyPlans">
-        <header>
-          <div className="flex justify-between items-center mb-6">
-            <h2 className="text-2xl mt-3">
-              {!renderPlanner ? `Training Plans` : `Create a Plan`}
-            </h2>
-            <button
-              className="btn-blue-light"
-              onClick={() =>
-                setRenderPlanner((prevRenderPlanner) => !prevRenderPlanner)
-              }
-            >
-              {!renderPlanner
-                ? `Create New Plan`
-                : `My Plans: ${savedPlans.length}`}
-            </button>
-          </div>
-        </header>
-        {!renderPlanner && (
-          <main className="plans--container">
-            {savedPlans.length === 0 && (
-              <h4>You currently have no training plans.</h4>
-            )}
-            <ul className="flex flex-col gap-6">
-              {savedPlans.map((plan) => (
-                <Plan
-                  plan={plan}
-                  key={plan._id}
-                  deletePlan={deletePlan}
-                />
-              ))}
-            </ul>
-          </main>
-        )}
-        {renderPlanner && (
-          <Planner
-            user={user}
-            setSavedPlans={setSavedPlans}
-            deletePlan={deletePlan}
-          />
-        )}
-      </div>
-    </div>
-  );
+		// Delete from savedEntries via setSavedEntries
+		setSavedPlans((prevSavedPlans) =>
+			prevSavedPlans.filter((plan) => planId !== plan._id)
+		);
+	};
+
+	return (
+		<div className="text-white mx-10">
+			<div className="MyPlans">
+				<header>
+					<div className="flex justify-between items-center mb-6">
+						<h2 className="text-2xl mt-3">
+							{!renderPlanner
+								? `Training Plans`
+								: `Create a Plan`}
+						</h2>
+						<button
+							className="btn-blue-light"
+							onClick={() =>
+								setRenderPlanner(
+									(prevRenderPlanner) => !prevRenderPlanner
+								)
+							}
+						>
+							{!renderPlanner
+								? `Create New Plan`
+								: `My Plans: ${savedPlans.length}`}
+						</button>
+					</div>
+				</header>
+				{!renderPlanner && (
+					<main className="plans--container">
+						{savedPlans.length === 0 && (
+							<h4>You currently have no training plans.</h4>
+						)}
+						<ul className="flex flex-col gap-6">
+							{savedPlans.map((plan) => (
+								<Plan
+									plan={plan}
+									key={plan._id}
+									deletePlan={deletePlan}
+									user={user}
+								/>
+							))}
+						</ul>
+					</main>
+				)}
+				{renderPlanner && (
+					<Planner
+						user={user}
+						setSavedPlans={setSavedPlans}
+						deletePlan={deletePlan}
+            fetchPlans={fetchPlans}
+					/>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default MyPlans;
--- server/routes/plansRoute.js
+++ server/routes/plansRoute.js
@@ -5,30 +5,33 @@ const Plan = require("../models/Plan");
 const client = "http://localhost:5173/fitness-app/";
 
 router.get("/:userId", async (req, res, next) => {
-  if (req.params.userId) {
-    const plans = await Plan.find({ userId: req.params.userId });
-    res.json({ plans });
-  }
+	if (req.params.userId) {
+		const plans = await Plan.find({ userId: req.params.userId });
+		res.json({ plans });
+	}
 });
 
 router.post("/", async (req, res, next) => {
-  if (req.body.userId) {
-    const plan = await Plan.create(req.body);
-    res.json({ status: "Success", plan });
-  }
+	if (req.body.userId) {
+		const plan = await Plan.create(req.body);
+		res.json({ status: "Success", plan });
+	}
 });
 
-router.delete("/:planId", async (req, res) => {
-  const { userId, planUserId } = req.body;
-  const { planId } = req.params;
+router.delete("/", async (req, res) => {
+	const { userId, planUserId, currentPlanId } = req.body;
 
-  if (userId == planUserId) {
-    const plan = await Plan.findByIdAndDelete(planId);
-    res.json({
-      status: "Successful",
-      plan,
-    });
-  }
+	if (userId == planUserId) {
+		try {
+			const plan = await Plan.findByIdAndDelete(currentPlanId);
+			res.json({
+				status: "Successful",
+				plan,
+			});
+		} catch (err) {
+			res.sendStatus(500);
+		}
+	}
 });
 
 module.exports = router;
