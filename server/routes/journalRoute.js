81a74372214ee0b2dc0af393d2b04830d8cac5cf
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-05 22:37:30 -0800

    Refactored code to be less wordy

--- server/routes/journalRoute.js
+++ server/routes/journalRoute.js
@@ -6,43 +6,30 @@ const { DateTime } = require("luxon");
 const client = "http://localhost:5173/fitness-app/journal";
 
 router.get("/", async (req, res) => {
-    const entries = await Entry.find();
-    res.json({
-        status: "Success",
-        entries,
-    });
+	const entries = await Entry.find();
+	res.json({
+		status: "Success",
+		entries,
+	});
 });
 
 router.post("/", async (req, res) => {
-    if (req.user) {
-        const entryDetail = {
-            author: req.user._id,
-            date: req.body.date,
-            plan: req.body.plan,
-            calories: req.body.calories,
-            macros: req.body.macros,
-            notes: req.body.notes,
-        };
-        const entry = new Entry(entryDetail).save((err) => {
-            if (err) {
-                next(err);
-            }
-        });
-        res.redirect(client);
-        // res.json({
-        //     status: "Success",
-        //     entry: entryDetail,
-        // });
-    }
+	if (req.body.userId) {
+		const entry = await Entry.create(req.body);
+		res.json({
+			status: "Success",
+			entry
+		});
+	}
 });
 
 router.delete("/:journalId", async (req, res) => {
-    const journalId = req.params.journalId;
-    const entry = await Entry.findByIdAndDelete(journalId);
-    res.json({
-        status: "Success",
-        entry,
-    });
+	const journalId = req.params.journalId;
+	const entry = await Entry.findByIdAndDelete(journalId);
+	res.json({
+		status: "Success",
+		entry,
+	});
 });
 
 module.exports = router;
