5f595612da81fa7367a233975251afe841b77072
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-05 22:38:16 -0800

    Changed author to userId to be similar to Plan

--- server/models/Entry.js
+++ server/models/Entry.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const EntrySchema = new Schema ({ 
-    author: { type: Schema.Types.ObjectId, ref: 'User' },
-    date: Date,
-    plan: String,
-    calories: String,
-    macros: String,
-    notes: String
-})
+const EntrySchema = new Schema({
+	userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+	date: Date,
+	plan: String,
+	calories: String,
+	macros: String,
+	notes: String,
+});
 
 module.exports = mongoose.model("Entry", EntrySchema);
