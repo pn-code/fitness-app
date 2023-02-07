3a9dc24d2504d10941a2873e4b56862c1be8325d
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-06 18:35:09 -0800

    Added redirect to landing page after log out

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import "../styles/profile.css";
 import axios from "axios";
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Profile = (props) => {
 	const { user, setUser } = props;
+	const navigate = useNavigate()
 
 	const handleLogOut = async () => {
 		axios({
 			method: "GET",
 			withCredentials: true,
 			url: "http://localhost:3000/log-out",
-		}).then((res) => setUser(res.data));
-		window.location.reload(true);
-	};
+		}).then((res) => setUser(res.data)).then(navigate("/fitness-app/landing"))
+	};	
 
 	return (
 		<div className="text-white mx-10 my-5 flex flex-col gap-2 sm:justify-center sm:items-center sm:mt-16">
@@ -22,7 +21,7 @@ const Profile = (props) => {
 				<header>
 					<h2 className="text-4xl font-bold">Profile</h2>
 					<span className="text-[#FDCA15]">
-						Hello, {user.first_name}.
+						Hello, {user?.first_name}.
 					</span>
 				</header>
 
