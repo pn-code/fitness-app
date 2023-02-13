e496ef696c08a796f378b8e9c5a3b7afb2f1fef8
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-13 15:59:55 -0800

    Added input handler and state

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,36 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post("http://localhost:3000/login", {
+                username,
+                password,
+            });
+            console.log(res);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
-        <div className="flex flex-col bg-[#040324] text-white px-10 pt-10 gap-2 items-center">
+        <div className="flex flex-col bg-[#040324] text-white px-10 pt-10 gap-2 items-center mt-10">
             <h1 className="text-3xl text-center">Login</h1>
             <form
+                onSubmit={handleSubmit}
                 className="bg-[#040324] flex flex-col items-center justify-center gap-3"
-                method="POST"
-                action={`http://localhost:3000/login`}
             >
                 <div className="flex flex-col">
                     <label htmlFor="username">Username: </label>
                     <input
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
                         className="input-bl"
                         id="username"
                         name="username"
@@ -22,6 +41,8 @@ const Login = () => {
                 <div className="flex flex-col">
                     <label htmlFor="password">Password: </label>
                     <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
                         className="input-bl"
                         id="password"
                         name="password"
