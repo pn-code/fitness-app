f11bc0053f0e607ada420a2f8d6f5ca3a9aa93cc
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-20 14:01:32 -0800

    Clean up code and added middleware

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://fitness.philipnguyen.dev"
+]
+
+module.exports = allowedOrigins;
---
+++ server/middleware/verifyJWT.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const verifyJWT = async (req, res, next) => {
+    // Check for auth header
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) return res.sendStatus(401);
+
+    // Get token from header
+    const token = authHeader.split(" ")[1];
+
+    // Verify the token
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) return res.sendStatus(403); // Forbidden / Invalid Token
+        req.user = decoded._id;
+        next();
+    });
+};
+
+module.exports = verifyJWT;
