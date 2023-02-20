61d9894b4e9cf2b6e0fb669f8822e9cb3231a2d5
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-19 18:55:00 -0800

    Added PUT endpoint

--- server/routes/plansRoute.js
+++ server/routes/plansRoute.js
@@ -3,33 +3,49 @@ const router = express.Router();
 const Plan = require("../models/Plan");
 
 router.get("/:userId", async (req, res, next) => {
-	if (req.params.userId) {
-		const plans = await Plan.find({ userId: req.params.userId });
-		res.json({ plans });
-	}
+  if (req.params.userId) {
+    const plans = await Plan.find({ userId: req.params.userId });
+    res.json({ plans });
+  }
 });
 
 router.post("/", async (req, res, next) => {
-	if (req.body.userId) {
-		const plan = await Plan.create(req.body);
-		res.json({ status: "Success", plan });
-	}
+  if (req.body.userId) {
+    const plan = await Plan.create(req.body);
+    res.json({ status: "Success", plan });
+  }
 });
 
 router.delete("/", async (req, res) => {
-	const { userId, planUserId, currentPlanId } = req.body;
+  const { userId, planUserId, currentPlanId } = req.body;
 
-	if (userId == planUserId) {
-		try {
-			const plan = await Plan.findByIdAndDelete(currentPlanId);
-			res.json({
-				status: "Successful",
-				plan,
-			});
-		} catch (err) {
-			res.sendStatus(500);
-		}
-	}
+  if (userId == planUserId) {
+    try {
+      const plan = await Plan.findByIdAndDelete(currentPlanId);
+      res.json({
+        status: "Successful",
+        plan,
+      });
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  }
+});
+
+router.put("/", async (req, res) => {
+  const { userId, planUserId, currentPlanId } = req.body;
+
+  if (userId == planUserId) {
+    try {
+      const plan = await Plan.findByIdAndUpdate(currentPlanId);
+      res.json({
+        status: "Successful",
+        plan,
+      });
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  }
 });
 
 module.exports = router;
