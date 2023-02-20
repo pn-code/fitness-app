4cdde2697ca5837ff88697811139e8434ac29b03
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-20 15:42:40 -0800

    Updated code to include user info

--- server/controllers/refreshTokenController.js
+++ server/controllers/refreshTokenController.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+require("dotenv").config();
 
 const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(401);
-
     const refreshToken = cookies.jwt;
 
-    const findUserByToken = await User.find({ refreshToken });
+    const findUserByToken = await User.findOne({ refreshToken: refreshToken });
 
     // If user with token is not found...
     if (!findUserByToken) return res.sendStatus(403); // Unauthorized
@@ -17,14 +17,20 @@ const handleRefreshToken = async (req, res) => {
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
-            if (err || findUserByToken._id !== decoded._id)
+            if (err || findUserByToken._id.toString() !== decoded._id)
                 return res.sendStatus(403);
             const accessToken = jwt.sign(
                 { _id: decoded._id },
-                process.env.ACCESS_TOKEN.SECRET,
+                process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: "1200s" }
             );
-            res.json({ accessToken });
+            res.json({
+                _id: findUserByToken._id,
+                firstName: findUserByToken.firstName,
+                lastName: findUserByToken.lastName,
+                email: findUserByToken.email,
+                accessToken,
+            });
         }
     );
 };
