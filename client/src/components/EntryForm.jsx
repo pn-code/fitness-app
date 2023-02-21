f4b09669a33fa7523c9b38def7af4ef0d8c5db4b
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-21 15:00:32 -0800

    Added exercise list render

--- client/src/components/EntryForm.jsx
+++ client/src/components/EntryForm.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { useEffect } from "react";
+import {
+    BsFillArrowUpCircleFill,
+    BsFillArrowDownCircleFill,
+} from "react-icons/bs";
 
 const EntryForm = ({
     user,
@@ -19,6 +23,7 @@ const EntryForm = ({
         macros: "",
         notes: "",
     });
+    const [viewAllExercises, setViewAllExercises] = useState(false);
 
     const planAPI = `${API_URL}/plans/`;
     const [plans, setPlans] = useState([]);
@@ -32,12 +37,6 @@ const EntryForm = ({
         });
         setPlans(res.data.plans);
     };
-
-    const renderPlanOptions = plans.map((plan) => (
-        <option key={plan._id} value={plan.title}>
-            {plan.title}
-        </option>
-    ));
 
     useEffect(() => {
         if (user._id) {
@@ -89,7 +88,9 @@ const EntryForm = ({
                 </h4>
                 <fieldset className="flex flex-col gap-2">
                     <div className="flex flex-col gap-1">
-                        <label htmlFor="date">Date: </label>
+                        <label className="text-[18px]" htmlFor="date">
+                            Date:{" "}
+                        </label>
                         <input
                             className="input-bl-lg w-72"
                             type="date"
@@ -102,7 +103,9 @@ const EntryForm = ({
                     </div>
 
                     <div className="flex flex-col gap-1">
-                        <label htmlFor="plan">Exercise Plan: </label>
+                        <label className="text-[18px]" htmlFor="plan">
+                            Exercise Plan:{" "}
+                        </label>
                         <select
                             className="input-bl-lg w-72"
                             id="plan"
@@ -111,12 +114,78 @@ const EntryForm = ({
                             value={entry.plan}
                         >
                             <option value="">NONE</option>
-                            {renderPlanOptions}
+                            {plans.map((plan) => (
+                                <option key={plan._id} value={plan._id}>
+                                    {plan.title}
+                                </option>
+                            ))}
                         </select>
                     </div>
 
+                    {entry.plan !== "" && (
+                        <div>
+                            <h4 className="text-[18px] flex gap-4">
+                                Exercises:{" "}
+                                <button
+                                    className="hover:scale-110 duration-150 ease-in"
+                                    onClick={() =>
+                                        setViewAllExercises(
+                                            (viewAllExercises) =>
+                                                !viewAllExercises
+                                        )
+                                    }
+                                    type="button"
+                                >
+                                    {viewAllExercises ? (
+                                        <BsFillArrowUpCircleFill size={24} />
+                                    ) : (
+                                        <BsFillArrowDownCircleFill size={24} />
+                                    )}
+                                </button>
+                            </h4>
+                            {viewAllExercises &&
+                                plans
+                                    .filter(
+                                        (plan) => plan._id === entry.plan
+                                    )[0]
+                                    .exercises.map((exercise) => (
+                                        <div className="mt-2" key={exercise.id}>
+                                            <h4 className="text-yellow-400">{exercise.name}</h4>
+                                            <div className="flex gap-8">
+                                                <div className="flex flex-col">
+                                                    <label htmlFor="sets">
+                                                        Sets:
+                                                    </label>
+                                                    <input
+                                                        className="input-bl w-24"
+                                                        id="sets"
+                                                        name="sets"
+                                                        type="text"
+                                                        placeholder="Sets"
+                                                    />
+                                                </div>
+                                                <div className="flex flex-col">
+                                                    <label htmlFor="reps">
+                                                        Reps:
+                                                    </label>
+                                                    <input
+                                                        className="input-bl w-24"
+                                                        id="reps"
+                                                        name="reps"
+                                                        type="text"
+                                                        placeholder="reps"
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
+                        </div>
+                    )}
+
                     <div className="flex flex-col gap-1">
-                        <label htmlFor="calories">Calorie Intake (kCal)</label>
+                        <label className="text-[18px]" htmlFor="calories">
+                            Calorie Intake (kCal)
+                        </label>
                         <input
                             className="input-bl-lg w-72"
                             type="number"
@@ -130,7 +199,7 @@ const EntryForm = ({
                     </div>
 
                     <div className="flex flex-col gap-1">
-                        <label htmlFor="macros">
+                        <label className="text-[18px]" htmlFor="macros">
                             Macronutrients (Optional):
                         </label>
                         <input
@@ -145,7 +214,9 @@ const EntryForm = ({
                     </div>
 
                     <div className="flex flex-col gap-1">
-                        <label id="notes">Notes: </label>
+                        <label className="text-[18px]" id="notes">
+                            Notes:{" "}
+                        </label>
                         <textarea
                             className="w-72 rounded-sm text-black p-2"
                             id="notes"
