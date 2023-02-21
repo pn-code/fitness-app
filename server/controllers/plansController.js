c54025237fda4189ab2b2ca8526cbdbc9034f5e9
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-21 14:07:23 -0800

    Updated updatePlan function

--- server/controllers/plansController.js
+++ server/controllers/plansController.js
@@ -1,49 +1,50 @@
 const Plan = require("../models/Plan");
 
 const getUserPlans = async (req, res, next) => {
-  if (req.params.userId) {
-    const plans = await Plan.find({ userId: req.params.userId });
-    res.json({ plans });
-  }
+    if (req.params.userId) {
+        const plans = await Plan.find({ userId: req.params.userId });
+        res.json({ plans });
+    }
 };
 
 const postPlan = async (req, res, next) => {
-  if (req.body.userId) {
-    const plan = await Plan.create(req.body);
-    res.json({ status: "Success", plan });
-  }
+    if (req.body.userId) {
+        const plan = await Plan.create(req.body);
+        res.json({ status: "Success", plan });
+    }
 };
 
 const deletePlan = async (req, res) => {
-  const { userId, planUserId, currentPlanId } = req.body;
+    const { userId, planUserId, currentPlanId } = req.body;
 
-  if (userId == planUserId) {
-    try {
-      const plan = await Plan.findByIdAndDelete(currentPlanId);
-      res.json({
-        status: "Successful",
-        plan,
-      });
-    } catch (err) {
-      res.sendStatus(500);
+    if (userId == planUserId) {
+        try {
+            const plan = await Plan.findByIdAndDelete(currentPlanId);
+            res.json({
+                status: "Successful",
+                plan,
+            });
+        } catch (err) {
+            res.sendStatus(500);
+        }
     }
-  }
 };
 
 const updatePlan = async (req, res) => {
-  const { userId, planUserId, currentPlanId } = req.body;
+    const { userId } = req.body;
+    const { planId } = req.params;
 
-  if (userId == planUserId) {
-    try {
-      const plan = await Plan.findByIdAndUpdate(currentPlanId, req.body);
-      res.json({
-        status: "Successful",
-        plan,
-      });
-    } catch (err) {
-      res.sendStatus(500);
+    if (req.user === userId) {
+        try {
+            const plan = await Plan.findByIdAndUpdate(planId, req.body);
+            res.json({
+                status: "Successful",
+                plan,
+            });
+        } catch (err) {
+            res.sendStatus(500);
+        }
     }
-  }
 };
 
 module.exports = { getUserPlans, postPlan, deletePlan, updatePlan };
