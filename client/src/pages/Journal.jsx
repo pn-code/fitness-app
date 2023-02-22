176d30ae281bd6728aa203e93b88b69143a7ff37
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-21 21:21:40 -0800

    Removing excess code and handling navigation

--- client/src/pages/Journal.jsx
+++ client/src/pages/Journal.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import "../styles/journal.css";
 import Entry from "../components/Entry";
-import EntryForm from "../components/EntryForm";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Journal = ({ user, API_URL }) => {
@@ -49,42 +48,25 @@ const Journal = ({ user, API_URL }) => {
     <div className="text-white mx-10 my-5 flex flex-col gap-2 justify-center sm:justify-left sm:mt-16">
       <div className="flex justify-between items-center mb-7">
         <h2 className="text-4xl font-bold">Journal</h2>
-        <button
-          className="btn-blue-light mt-0"
-          type="button"
-          onClick={() => setAddEntry((prevState) => !prevState)}
-        >
-          {addEntry ? "Back" : "Add Entry"}
-        </button>
+        <Link className="btn-blue" to="/add-entry" state={plans}>
+          Add Entry
+        </Link>
       </div>
 
-      {addEntry && (
-        <EntryForm
-          user={user}
-          setEntries={setEntries}
-          API_URL={API_URL}
-          journalAPI={journalAPI}
-          setAddEntry={setAddEntry}
-          fetchEntries={fetchEntries}
-          plans={plans}
-        />
-      )}
-      {!addEntry && (
-        <main className="journal-entries--container">
-          {entries.length === 0 && <h4>You have no saved journal entries.</h4>}
-          {entries.length > 0 &&
-            entries.map((entry, idx) => (
-              <Entry
-                user={user}
-                entry={entry}
-                plans={plans}
-                key={entry._id || idx}
-                setEntries={setEntries}
-                journalAPI={journalAPI}
-              />
-            ))}
-        </main>
-      )}
+      <main className="flex flex-col gap-4">
+        {entries.length === 0 && <h4>You have no saved journal entries.</h4>}
+        {entries.length > 0 &&
+          entries.map((entry, idx) => (
+            <Entry
+              user={user}
+              entry={entry}
+              plans={plans}
+              key={entry._id || idx}
+              setEntries={setEntries}
+              journalAPI={journalAPI}
+            />
+          ))}
+      </main>
     </div>
   );
 };
