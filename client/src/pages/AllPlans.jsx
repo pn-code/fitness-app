fba8db2e605947727668d348752dca6db84594f2
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-22 16:14:48 -0800

    Added search filter function

--- client/src/pages/AllPlans.jsx
+++ client/src/pages/AllPlans.jsx
@@ -1,14 +1,24 @@
-import React from "react";
-import { useEffect } from "react";
-import Plan from "../components/Plan";
-import Planner from "./Planner";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { BiSearch } from "react-icons/bi";
+import Plan from "../components/Plan";
 
 const AllPlans = ({ user, API_URL }) => {
-    const [allPlans, setAllPlans] = React.useState([]);
-
     const planAPI = `${API_URL}/plans/`;
+
+    const [allPlans, setAllPlans] = useState([]);
+    const [searchFilter, setSearchFilter] = useState("");
+    const [filteredPlans, setFilteredPlans] = useState([]);
+
+    const applySearchFilter = () => {
+        const regex = new RegExp(searchFilter, "i");
+        const filterAllPlans = allPlans.filter(({ title, emphasis, desc }) =>
+            [title, emphasis, desc].some((item) => item.match(regex))
+        );
+
+        setFilteredPlans(filterAllPlans);
+    };
 
     const fetchPlans = async () => {
         const res = await axios.get(`${planAPI}`, {
@@ -18,9 +28,9 @@ const AllPlans = ({ user, API_URL }) => {
             },
         });
         setAllPlans(res.data.plans);
+        setFilteredPlans(res.data.plans);
     };
 
-    // Loads allPlans on start with information from database
     useEffect(() => {
         if (user) {
             fetchPlans();
@@ -31,8 +41,9 @@ const AllPlans = ({ user, API_URL }) => {
         <div>
             <div className="text-white mx-10 my-5 flex flex-col gap-2 justify-center sm:justify-left sm:mt-16">
                 <div className="mb-5">
-                    <div className="flex justify-between items-center mb-7">
+                    <div className="flex justify-between items-center mb-4">
                         <h2 className="text-4xl font-bold">All Plans</h2>
+
                         <div className="flex gap-4">
                             <Link
                                 to="/my-plans"
@@ -43,12 +54,30 @@ const AllPlans = ({ user, API_URL }) => {
                         </div>
                     </div>
 
+                    <div className="flex items-center justify-center mb-8 gap-2">
+                        <input
+                            onChange={(e) => setSearchFilter(e.target.value)}
+                            value={searchFilter}
+                            placeholder="Search..."
+                            id="search"
+                            name="search"
+                            className="input-bl w-[92%] md:max-w-[800px] h-10 bg-blue-900 text-white placeholder-slate-300 mt-4"
+                        />
+                        <button
+                            onClick={applySearchFilter}
+                            type="button"
+                            className="btn-blue-light w-16 justify-center items-center"
+                        >
+                            <BiSearch className="text-center" size={26} />
+                        </button>
+                    </div>
+
                     <main className="plans--container">
                         {allPlans.length === 0 && (
                             <h4>You currently have no training plans.</h4>
                         )}
                         <ul className="flex flex-col gap-6">
-                            {allPlans.map((plan) => (
+                            {filteredPlans.map((plan) => (
                                 <Plan plan={plan} key={plan._id} user={user} />
                             ))}
                         </ul>
