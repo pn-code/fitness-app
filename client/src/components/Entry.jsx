32c657159f66e2a3590bb0a61d33f81d54b76519
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-23 12:11:09 -0800

    Implemented deleteModal for entry

--- client/src/components/Entry.jsx
+++ client/src/components/Entry.jsx
@@ -2,105 +2,128 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { DateTime } from "luxon";
 import axios from "axios";
+import DeleteModal from "../components/DeleteModal";
 
 const Entry = ({ user, entry, plans, setEntries, journalAPI }) => {
-  const [viewEntry, setViewEntry] = useState(false);
-  const formattedDate = DateTime.fromISO(entry.date).toFormat("LLL dd, yyyy");
+    const [viewEntry, setViewEntry] = useState(false);
+    const [openDeleteModal, setOpenDeleteModal] = useState(false);
 
-  const deleteEntry = async (id) => {
-    // findAndDelete from db
-    await axios.delete(journalAPI + id, {
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${user.accessToken}`,
-      },
-    });
+    const formattedDate = DateTime.fromISO(entry.date).toFormat("LLL dd, yyyy");
 
-    // Remove from entries array
-    setEntries((prevEntries) => prevEntries.filter((entry) => id != entry._id));
-  };
+    const deleteEntry = async (e, id) => {
+        e.preventDefault();
 
-  return (
-    <div className="flex flex-col gap-2 bg-gray-700 px-5 py-4 rounded-md">
-      <section className="date--container">
-        <h4 className="text-lg font-semibold mb-2">{formattedDate}</h4>
-      </section>
+        await axios.delete(journalAPI + id, {
+            withCredentials: true,
+            headers: {
+                Authorization: `Bearer ${user.accessToken}`,
+            },
+        });
 
-      <section className="flex flex-col sm:flex-row justify-between sm:gap-2">
-        <div className="flex gap-2 items-center my-2">
-          <h3 className="font-semibold">Calories:</h3> {entry.calories} Cal
-        </div>
-        <div className="flex gap-2">
-          <h3 className="font-semibold">Macros (C/F/P): </h3>
-          <span>{entry.macros}</span>
-        </div>
-      </section>
+        setEntries((prevEntries) =>
+            prevEntries.filter((entry) => id != entry._id)
+        );
+    };
 
-      {!viewEntry && (
-        <div
-          onClick={() => setViewEntry(true)}
-          className="text-xs text-center mt-4 cursor-pointer"
-        >
-          <span>Click to View</span>
-        </div>
-      )}
-
-      {viewEntry && (
-        <>
-          <section>
-            <div className="flex flex-col sm:items-center sm:flex-row gap-2 my-2">
-              <h3 className="font-semibold"> Exercise Plan: </h3>
-              <span>{entry.plan === "" ? "NONE SELECTED" : entry.plan}</span>
-            </div>
-
-            {entry.exercises.length > 0 && (
-              <div className="my-4">
-                <h3 className="font-semibold mb-2">Exercises: </h3>
-                {entry.exercises.map((exercise) => (
-                  <div key={exercise.id}>
-                    <h4 className="text-yellow-400">{exercise.name}</h4>
-                    <span>
-                      {exercise.sets} sets of {exercise.reps} reps @ {exercise.weight}
-                    </span>
-                  </div>
-                ))}
-              </div>
+    return (
+        <div className="flex flex-col gap-2 bg-gray-700 px-5 py-4 rounded-md">
+            {openDeleteModal && (
+                <DeleteModal
+                    action={(e) => deleteEntry(e, entry._id)}
+                    closeModal={() => setOpenDeleteModal(false)}
+                />
             )}
-          </section>
 
-          <section className="notes--container">
-            <div>
-              <h3 className="font-semibold">Notes: </h3>
-              <span>{entry.notes}</span>
-            </div>
-          </section>
-          <div className="flex justify-between gap-[16%] mt-5">
-            <Link
-              className="bg-green-600 py-4 rounded-md hover:bg-green-400 text-center flex-1"
-              to={`/edit-entry/${entry._id}`}
-              state={{entry, plans}}
-            >
-              Edit
-            </Link>
-            <button
-              className="bg-red-500 py-4 rounded-md hover:bg-red-400 flex-1"
-              onClick={() => deleteEntry(entry._id)}
-            >
-              Remove
-            </button>
-          </div>
-          {viewEntry && (
-            <div
-              onClick={() => setViewEntry(false)}
-              className="text-xs text-center mt-8 cursor-pointer"
-            >
-              <span>Click to Close</span>
-            </div>
-          )}
-        </>
-      )}
-    </div>
-  );
+            <section className="date--container">
+                <h4 className="text-lg font-semibold mb-2">{formattedDate}</h4>
+            </section>
+
+            <section className="flex flex-col sm:flex-row justify-between sm:gap-2">
+                <div className="flex gap-2 items-center my-2">
+                    <h3 className="font-semibold">Calories:</h3>{" "}
+                    {entry.calories} Cal
+                </div>
+                <div className="flex gap-2">
+                    <h3 className="font-semibold">Macros (C/F/P): </h3>
+                    <span>{entry.macros}</span>
+                </div>
+            </section>
+
+            {!viewEntry && (
+                <div
+                    onClick={() => setViewEntry(true)}
+                    className="text-xs text-center mt-4 cursor-pointer"
+                >
+                    <span>Click to View</span>
+                </div>
+            )}
+
+            {viewEntry && (
+                <>
+                    <section>
+                        <div className="flex flex-col sm:items-center sm:flex-row gap-2 my-2">
+                            <h3 className="font-semibold"> Exercise Plan: </h3>
+                            <span>
+                                {entry.plan === ""
+                                    ? "NONE SELECTED"
+                                    : entry.plan}
+                            </span>
+                        </div>
+
+                        {entry.exercises.length > 0 && (
+                            <div className="my-4">
+                                <h3 className="font-semibold mb-2">
+                                    Exercises:{" "}
+                                </h3>
+                                {entry.exercises.map((exercise) => (
+                                    <div key={exercise.id}>
+                                        <h4 className="text-yellow-400">
+                                            {exercise.name}
+                                        </h4>
+                                        <span>
+                                            {exercise.sets} sets of{" "}
+                                            {exercise.reps} reps @{" "}
+                                            {exercise.weight}
+                                        </span>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </section>
+
+                    <section className="notes--container">
+                        <div>
+                            <h3 className="font-semibold">Notes: </h3>
+                            <span>{entry.notes}</span>
+                        </div>
+                    </section>
+                    <div className="flex justify-between gap-[16%] mt-5">
+                        <Link
+                            className="bg-green-600 py-4 rounded-md hover:bg-green-400 text-center flex-1"
+                            to={`/edit-entry/${entry._id}`}
+                            state={{ entry, plans }}
+                        >
+                            Edit
+                        </Link>
+                        <button
+                            className="bg-red-500 py-4 rounded-md hover:bg-red-400 flex-1"
+                            onClick={() => setOpenDeleteModal(true)}
+                        >
+                            Remove
+                        </button>
+                    </div>
+                    {viewEntry && (
+                        <div
+                            onClick={() => setViewEntry(false)}
+                            className="text-xs text-center mt-8 cursor-pointer"
+                        >
+                            <span>Click to Close</span>
+                        </div>
+                    )}
+                </>
+            )}
+        </div>
+    );
 };
 
 export default Entry;
