6f2320dcb3f1c1c87928b792d1fed344f1c7bda5
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-23 11:57:06 -0800

    Edited styles to center  & passed actions to btns

--- client/src/components/DeleteModal.jsx
+++ client/src/components/DeleteModal.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 
-const DeleteModal = ({ action }) => {
+const DeleteModal = ({ action, closeModal }) => {
     return (
-        <div className="w-[460px] h-full relative">
-            <form className="bg-slate-700 text-white p-12 absolute top-[50%] left-[50%] rounded-md w-full">
+        <div className="absolute left-0 right-0 top-[30%] bottom-0 m-auto w-[300px] z-[999]">
+            <form
+                onSubmit={action}
+                className="bg-slate-900 text-white p-12 rounded-md"
+            >
                 <h1 className="mb-2">
                     Are you sure you want to delete this plan/entry?
                 </h1>
-                <section className="flex justify-between gap-12">
-                    <button className="btn-blue bg-gray-500">Cancel</button>
-                    <button className="btn-blue bg-red-600">Delete</button>
+                <section className="flex flex-col justify-between sm:gap-12 sm:flex-row">
+                    <button
+                        onClick={closeModal}
+                        type="button"
+                        className="btn-blue bg-gray-500"
+                    >
+                        Cancel
+                    </button>
+                    <button type="submit" className="btn-blue bg-red-600">
+                        Delete
+                    </button>
                 </section>
             </form>
         </div>
