bf2e1c7c17db09eb80871bcdbc97481158f16cce
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-22 22:12:27 -0800

    Added styles to modal

--- client/src/components/DeleteModal.jsx
+++ client/src/components/DeleteModal.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const DeleteModal = ({itemType, title}) => {
-  return (
-    <form>
-        <h1>Are you sure you want to delete this plan/entry?</h1>
-        <section>
-        <button>Delete</button>
-        <button>Cancel</button>
-        </section>
-    </form>
-  )
-}
+const DeleteModal = ({ action }) => {
+    return (
+        <div className="w-[460px] h-full relative">
+            <form className="bg-slate-700 text-white p-12 absolute top-[50%] left-[50%] rounded-md w-full">
+                <h1 className="mb-2">
+                    Are you sure you want to delete this plan/entry?
+                </h1>
+                <section className="flex justify-between gap-12">
+                    <button className="btn-blue bg-gray-500">Cancel</button>
+                    <button className="btn-blue bg-red-600">Delete</button>
+                </section>
+            </form>
+        </div>
+    );
+};
 
-export default DeleteModal
+export default DeleteModal;
