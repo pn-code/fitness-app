d17a4d045067bb354aabda80c9ad58ebe1b13ed9
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-22 21:27:21 -0800

    Adding savedPlans section

--- client/src/pages/MyPlans.jsx
+++ client/src/pages/MyPlans.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import Plan from "../components/Plan";
 import Planner from "./Planner";
@@ -6,19 +6,25 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const MyPlans = ({ user, API_URL }) => {
-    const [userPlans, setUserPlans] = React.useState([]);
-    const [renderPlanner, setRenderPlanner] = React.useState(false);
+    const [userPlans, setUserPlans] = useState([]);
+    const [savedPlans, setSavedPlans] = useState([]);
+    const [renderPlanner, setRenderPlanner] = useState(false);
 
     const planAPI = `${API_URL}/plans/`;
 
     const fetchPlans = async () => {
-        const res = await axios.get(`${planAPI}${user?._id}`, {
+        const res = await axios.get(`${planAPI}`, {
             withCredentials: true,
             headers: {
                 Authorization: `Bearer ${user.accessToken}`,
             },
         });
-        setUserPlans(res.data.plans);
+        setUserPlans(res.data.plans.filter((plan) => plan.userId === user._id));
+        setSavedPlans(
+            res.data.plans
+                .filter((plan) => plan.saved.includes(user._id))
+                .filter((plan) => plan.userId !== user._id)
+        );
     };
 
     useEffect(() => {
@@ -60,14 +66,20 @@ const MyPlans = ({ user, API_URL }) => {
                                         ? `New Plan`
                                         : `My Plans: ${userPlans.length}`}
                                 </button>
-                                <Link to="/all-plans" className="btn-blue mt-0 text-center">
+                                <Link
+                                    to="/all-plans"
+                                    className="btn-blue mt-0 text-center"
+                                >
                                     View All
                                 </Link>
                             </div>
                         </div>
 
                         {!renderPlanner && (
-                            <main className="plans--container">
+                            <section className="plans--container">
+                                <h2 className="text-purple-300 font-bold my-4 text-lg">
+                                    My Plans
+                                </h2>
                                 {userPlans.length === 0 && (
                                     <h4>
                                         You currently have no training plans.
@@ -83,7 +95,31 @@ const MyPlans = ({ user, API_URL }) => {
                                         />
                                     ))}
                                 </ul>
-                            </main>
+                            </section>
+                        )}
+
+                        {!renderPlanner && (
+                            <section className="plans--container">
+                                <h2 className="text-purple-300 font-bold my-4 text-lg">
+                                    Saved Plans
+                                </h2>
+                                {savedPlans.length === 0 && (
+                                    <h4>
+                                        You currently have no saved training
+                                        plans.
+                                    </h4>
+                                )}
+                                <ul className="flex flex-col gap-6">
+                                    {savedPlans.map((plan) => (
+                                        <Plan
+                                            plan={plan}
+                                            key={plan._id}
+                                            user={user}
+                                            planAPI={planAPI}
+                                        />
+                                    ))}
+                                </ul>
+                            </section>
                         )}
                     </div>
                 </div>
