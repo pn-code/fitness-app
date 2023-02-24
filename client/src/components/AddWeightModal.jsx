a06633b57d97197119b4cd1908d1cd7bd6cab3f0
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-23 19:22:45 -0800

    Added submitWeight handler

--- client/src/components/AddWeightModal.jsx
+++ client/src/components/AddWeightModal.jsx
@@ -1,15 +1,61 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 
-const AddWeightModal = ({ action, closeModal }) => {
-    const [currentWeight, setCurrentWeight] = useState("")
-    const [checkedUnitInput, setCheckedUnitInput] = useState("pounds")
+const AddWeightModal = ({ closeModal, API_URL, user, setUser }) => {
+    const [currentWeight, setCurrentWeight] = useState("");
+    const [checkedUnitInput, setCheckedUnitInput] = useState("pounds");
 
-console.log(checkedUnitInput)
+    const handleSubmitWeight = async (e) => {
+        e.preventDefault();
+        if (user?._id) {
+            try {
+                const weightObj =
+                    checkedUnitInput === "pounds"
+                        ? {
+                              pounds: currentWeight,
+                              kilograms: currentWeight / 2.205,
+                              date: new Date(),
+                          }
+                        : {
+                              pounds: currentWeight * 2.205,
+                              kilograms: currentWeight,
+                              date: new Date(),
+                          };
+
+                const newWeightsArray = {
+                    weights: [...user?.weights, weightObj],
+                };
+
+                await axios.put(
+                    `${API_URL}/user/${user._id}`,
+                    newWeightsArray,
+                    {
+                        withCredentials: true,
+                        headers: {
+                            Authorization: `Bearer ${user.accessToken}`,
+                        },
+                    }
+                );
+
+                setUser(user => ({
+                    ...user, 
+                    weights: [...user?.weights, weightObj]
+                }))
+
+                closeModal()
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
+    console.log(user)
+
     return (
         <div className="absolute left-0 right-0 top-[30%] bottom-0 m-auto w-[320px] z-[999]">
             <form
-                onSubmit={action}
+                onSubmit={(e) => handleSubmitWeight(e)}
                 className="bg-slate-900 text-white p-12 rounded-md"
             >
                 <h1 className="text-2xl font-bold mb-2">Weight</h1>
@@ -18,6 +64,8 @@ console.log(checkedUnitInput)
                     <section>
                         <label htmlFor="current-weight">Current Weight: </label>
                         <input
+                            onChange={(e) => setCurrentWeight(e.target.value)}
+                            value={currentWeight}
                             className="input-bl w-36"
                             type="number"
                             placeholder="Current Weight"
@@ -28,7 +76,9 @@ console.log(checkedUnitInput)
                         <div className="flex items-center">
                             <input
                                 defaultChecked
-                                onClick={(e) => setCheckedUnitInput(e.target.value)}
+                                onClick={(e) =>
+                                    setCheckedUnitInput(e.target.value)
+                                }
                                 name="unit"
                                 className="mr-2 w-4 h-4"
                                 type="radio"
@@ -43,7 +93,9 @@ console.log(checkedUnitInput)
                                 name="unit"
                                 className="mr-2 w-4 h-4"
                                 type="radio"
-                                onClick={(e) => setCheckedUnitInput(e.target.value)}
+                                onClick={(e) =>
+                                    setCheckedUnitInput(e.target.value)
+                                }
                                 value="kilograms"
                                 id="kilograms"
                             />
