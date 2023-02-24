d93738eca4c65e963f19b1cbbab976577724bcc9
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-23 19:24:33 -0800

    Added different cases for changes to user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,12 +4,25 @@ require("dotenv").config();
 const updateUserMeasurements = async (req, res) => {
     const { userId } = req.params;
     const { weights, calorieGoal } = req.body;
+
     if (req.user === userId) {
-        const updateUser = await User.findByIdAndUpdate(userId, {
-            weights,
-            calorieGoal,
-        });
-        res.status(201).json({ success: true, user: updateUser });
+        if (weights && calorieGoal) {
+            const updateUser = await User.findByIdAndUpdate(userId, {
+                weights,
+                calorieGoal,
+            });
+            res.status(201).json({ success: true, user: updateUser });
+        } else if (weights) {
+            const updateUser = await User.findByIdAndUpdate(userId, {
+                weights,
+            });
+            res.status(201).json({ success: true, user: updateUser });
+        } else if (calorieGoal) {
+            const updateUser = await User.findByIdAndUpdate(userId, {
+                calorieGoal,
+            });
+            res.status(201).json({ success: true, user: updateUser });
+        }
     }
 };
 
