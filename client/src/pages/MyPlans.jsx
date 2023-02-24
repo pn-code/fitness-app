e62edd12ca0f4f4a18f51d4ad7e1d5da57c7c091
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-24 14:51:50 -0800

    Removing planner from MyPlans page

--- client/src/pages/MyPlans.jsx
+++ client/src/pages/MyPlans.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Plan from "../components/Plan";
-import Planner from "./Planner";
 import serverAPI from "../api/serverAPI";
 
 const MyPlans = ({ user }) => {
@@ -31,93 +30,66 @@ const MyPlans = ({ user }) => {
 
     return (
         <div>
-            {renderPlanner ? (
-                <Planner
-                    user={user}
-                    fetchPlans={fetchPlans}
-                    setRenderPlanner={setRenderPlanner}
-                    renderPlanner={renderPlanner}
-                    userPlans={userPlans}
-                    setUserPlans={setUserPlans}
-                />
-            ) : (
-                <div className="text-white mx-10 my-5 flex flex-col gap-2 justify-center sm:justify-left sm:mt-16">
-                    <div className="mb-5">
-                        <div className="flex justify-between items-center gap-4">
-                            <h2 className="text-3xl font-bold">
-                                {!renderPlanner && "Plans"}
-                            </h2>
-                            <div className="flex gap-4">
-                                <button
-                                    className="btn-blue-light mt-0"
-                                    onClick={() =>
-                                        setRenderPlanner(
-                                            (prevRenderPlanner) =>
-                                                !prevRenderPlanner
-                                        )
-                                    }
-                                >
-                                    {!renderPlanner
-                                        ? `New Plan`
-                                        : `My Plans: ${userPlans.length}`}
-                                </button>
-                                <Link
-                                    to="/all-plans"
-                                    className="btn-blue mt-0 text-center"
-                                >
-                                    View All
-                                </Link>
-                            </div>
+            {/* <Planner
+                user={user}
+                fetchPlans={fetchPlans}
+                setRenderPlanner={setRenderPlanner}
+                renderPlanner={renderPlanner}
+                userPlans={userPlans}
+                setUserPlans={setUserPlans}
+            /> */}
+
+            <div className="text-white mx-10 my-5 flex flex-col gap-2 justify-center sm:justify-left sm:mt-16">
+                <div className="mb-5">
+                    <div className="flex justify-between items-center gap-4">
+                        <h2 className="text-3xl font-bold">Plans</h2>
+                        <div className="flex gap-4">
+                            <Link to="/planner" className="btn-blue-light mt-0">
+                                New Plan
+                            </Link>
+                            <Link
+                                to="/all-plans"
+                                className="btn-blue mt-0 text-center"
+                            >
+                                View All
+                            </Link>
                         </div>
-
-                        {!renderPlanner && (
-                            <section className="plans--container">
-                                <h2 className="text-purple-300 font-bold my-4 text-lg">
-                                    My Plans
-                                </h2>
-                                {userPlans.length === 0 && (
-                                    <h4>
-                                        You currently have no training plans.
-                                    </h4>
-                                )}
-                                <ul className="flex flex-col gap-6">
-                                    {userPlans.map((plan) => (
-                                        <Plan
-                                            plan={plan}
-                                            key={plan._id}
-                                            user={user}
-                                            setUserPlans={setUserPlans}
-                                        />
-                                    ))}
-                                </ul>
-                            </section>
-                        )}
-
-                        {!renderPlanner && (
-                            <section className="plans--container">
-                                <h2 className="text-purple-300 font-bold my-4 text-lg">
-                                    Saved Plans
-                                </h2>
-                                {savedPlans.length === 0 && (
-                                    <h4>
-                                        You currently have no saved training
-                                        plans.
-                                    </h4>
-                                )}
-                                <ul className="flex flex-col gap-6">
-                                    {savedPlans.map((plan) => (
-                                        <Plan
-                                            plan={plan}
-                                            key={plan._id}
-                                            user={user}
-                                        />
-                                    ))}
-                                </ul>
-                            </section>
-                        )}
                     </div>
+
+                    <section className="plans--container">
+                        <h2 className="text-purple-300 font-bold my-4 text-lg">
+                            My Plans
+                        </h2>
+                        {userPlans.length === 0 && (
+                            <h4>You currently have no training plans.</h4>
+                        )}
+                        <ul className="flex flex-col gap-6">
+                            {userPlans.map((plan) => (
+                                <Plan
+                                    plan={plan}
+                                    key={plan._id}
+                                    user={user}
+                                    setUserPlans={setUserPlans}
+                                />
+                            ))}
+                        </ul>
+                    </section>
+
+                    <section className="plans--container">
+                        <h2 className="text-purple-300 font-bold my-4 text-lg">
+                            Saved Plans
+                        </h2>
+                        {savedPlans.length === 0 && (
+                            <h4>You currently have no saved training plans.</h4>
+                        )}
+                        <ul className="flex flex-col gap-6">
+                            {savedPlans.map((plan) => (
+                                <Plan plan={plan} key={plan._id} user={user} />
+                            ))}
+                        </ul>
+                    </section>
                 </div>
-            )}
+            </div>
         </div>
     );
 };
