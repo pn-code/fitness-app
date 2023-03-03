03b04af674d019e62c6fe4058834e902b72d24ab
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-02 20:02:09 -0800

    Adding info to Exercise Selection section

--- client/src/pages/FAQs.jsx
+++ client/src/pages/FAQs.jsx
@@ -28,7 +28,9 @@ const FAQs = () => {
                         <div className="flex flex-col gap-2 text-[16px]">
                             <h5>Simply follow this QUICK 3-STEP GUIDE:</h5>
 
-                            <h6 className="underline">Step 1: Set your weight & daily goal.</h6>
+                            <h6 className="underline">
+                                Step 1: Set your weight & daily goal.
+                            </h6>
 
                             <p className="block text-gray-300 text-[15px]">
                                 Click on weight/daily goal by the top-left
@@ -36,12 +38,16 @@ const FAQs = () => {
                                 plan.
                             </p>
 
-                            <h6 className="underline">Step 2: Add or create an exercise plan.</h6>
+                            <h6 className="underline">
+                                Step 2: Add or create an exercise plan.
+                            </h6>
                             <p className="block text-gray-300 text-[15px]">
                                 Visit the PLANS link to add/create a plan.
                             </p>
 
-                            <h5 className="underline">Step 3: Utilize the journal.</h5>
+                            <h5 className="underline">
+                                Step 3: Utilize the journal.
+                            </h5>
                             <p className="block text-gray-300 text-[15px]">
                                 Visit the JOURNAL link to add entries, fill out
                                 exercise plans done, and calories consumed.
@@ -53,9 +59,69 @@ const FAQs = () => {
                         <h4 className="text-xl text-yellow-400 mb-2">
                             Exercise Selection
                         </h4>
-                        <p className="text-[16px]">
-                            Head over to our exercise explorer.
-                        </p>
+                        <section className="text-[16px] flex flex-col gap-2">
+                            <p>
+                                Exercise selection can be simple, but can be
+                                overcomplicated quickly. If you do not know what
+                                exercises to choose and how to put together a
+                                plan, read this before moving forward.
+                            </p>
+
+                            <h6 className="mt-2">
+                                Choose 1-2 movements for each muscle group
+                                listed below.
+                            </h6>
+
+                            <ul className="list-disc mx-5">
+                                <li>Back</li>
+                                <li>Chest & Shoulders</li>
+                                <li>Hamstrings & Glutes</li>
+                                <li>Quadriceps</li>
+                                <li>(Optional) Abdominals</li>
+                                <li>(Optional) Biceps</li>
+                                <li>(Optional) Triceps</li>
+                                <li>(Optional) Calves</li>
+                                <li>(Optional) Forearms</li>
+                                <li>(Optional) Neck</li>
+                            </ul>
+
+                            <p>
+                                As you can see there are several large muscle
+                                groups that you should target. There are also
+                                smaller muscle groups I've included that are
+                                optional. Please feel free to include other
+                                muscle groups you may feel are important.
+                            </p>
+
+                            <p>
+                                Here's a simple exercise plan I've created from
+                                the muscle groups I deemed mandatory above:
+                            </p>
+
+                            <ul className="list-disc mx-5">
+                                <li>
+                                    <h6>Back:</h6>
+                                    <span>Pull-ups & Rows</span>
+                                </li>
+                                <li>
+                                    <h6>Chest & Shoulders:</h6>
+                                    <span>Bench Press & Overhead Press</span>
+                                </li>
+                                <li>
+                                    <h6>Hamstrings & Glutes:</h6>
+                                    <span>Deadlifts & Leg Curls</span>
+                                </li>
+                                <li>
+                                    <h6>Quadriceps:</h6>
+                                    <span>Back Squats</span>
+                                </li>
+                            </ul>
+
+                            <p>
+                                Now that you have your exercises. You can set
+                                them up to fit into your desired training split.
+                            </p>
+                        </section>
                     </article>
                 </section>
 
