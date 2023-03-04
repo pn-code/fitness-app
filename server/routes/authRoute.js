0e90cce8c1c13df98a95675e13a17c841f313238
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-03 17:40:00 -0800

    Implementing Guess Login

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {
     registerUser,
     loginUser,
+    loginAsGuest,
     logoutUser,
     getUserData,
 } = require("../controllers/authController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/guest", loginAsGuest);
 router.get("/user", getUserData);
 router.get("/logout", logoutUser);
 
